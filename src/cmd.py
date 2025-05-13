0ee287dd5ef24f28ba875b7d6ad762103e0c204d
Ragamafia <pragim.atat@mail.ru>
2025-05-13 21:35:59 +0800

    saved change

--- src/cmd.py
+++ src/cmd.py
@@ -14,9 +14,13 @@ class Employer:
     hours_worked: int
     rate: int
 
+
 class ReportModule:
     type: str
     name: str
+
+    def print_report(self, files: list[Employer]):
+        ...
 
 
 class PayoutReportModule(ReportModule):
@@ -24,7 +28,7 @@ class PayoutReportModule(ReportModule):
     name: str = "Отчет по зарплате"
 
     def print_report(self, files: list[Employer]):
-        print(f"{'name':>19} {'hours':>18} {'rate':>10} {'payout':>13}")
+        print(f"{'name':>20} {'hours':>20} {'rate':>13} {'payout':>18}")
         departments = []
         total_cash = 0
         total_hours = 0
@@ -36,17 +40,17 @@ class PayoutReportModule(ReportModule):
 
             cash = int(employee.rate) * int(employee.hours_worked)
             print(f"{' ':->14}",
-                  f"{employee.name:<18}",
-                  f"{employee.hours_worked:<12}",
-                  f"{employee.rate:<10}",
+                  f"{employee.name:<20}",
+                  f"{employee.hours_worked:<15}",
+                  f"{employee.rate:<15}",
                   f"${cash:<10}"
                   )
 
             total_cash += cash
             total_hours+= int(employee.hours_worked)
 
-        print(f"{total_hours:>37}{'$':>22}{total_cash:<7}")
-        print(f"{'':->65}")
+        print(f"{total_hours:>39}{'$':>30}{total_cash:<7}")
+        print(f"{'':_>75}")
 
 
 @dataclass
